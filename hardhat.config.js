5a8ce8ac256984d228d1f2b5329a2ce788233662
seshanthS <>
2021-09-24 20:13:09 +0530

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,50 @@
 require("@nomiclabs/hardhat-waffle");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
+require("@nomiclabs/hardhat-solhint");
+require("dotenv").config();
+require("solidity-coverage");
+require('@openzeppelin/hardhat-upgrades');
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.ALCHEMY_URL_MAINNET,
+        blockNumber: 10984219//13055980,
+      },
+    },
+
+    mainnet: {
+      url: process.env.ALCHEMY_URL_MAINNET,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+    // kovan: {
+    //   url: process.env.ALCHEMY_URL_KOVAN,
+    //   accounts: [`0x${process.env.PRIVATE_KEY}`],
+    // },
+    // rinkeby: {
+    //   url: process.env.ALCHEMY_URL_RINKEBY,
+    //   accounts: [`0x${process.env.PRIVATE_KEY}`],
+    // },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  solidity: {
+    version: "0.8.7",//"0.8.7",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  mocha: {
+    timeout: 700000000
+  },
 };
