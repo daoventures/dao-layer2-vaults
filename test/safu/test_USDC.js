5c469e31ac15b0b9c22c98ee0746588ee38693bd
seshanthS <>
2021-10-01 10:09:45 +0530

    update priceIndex in test

--- test/safu/test_USDC.js
+++ test/safu/test_USDC.js
@@ -189,9 +189,9 @@ describe("Citadel - USDC", async () => {
         console.log("USER 3 LP Tokens", (await vault.balanceOf(user3.address)).toString())
 
 
-        await vault.connect(user1).withdraw(await vault.balanceOf(user1.address), USDC.address, [0,0,0,0])
-        await vault.connect(user2).withdraw(await vault.balanceOf(user2.address), USDC.address, [0,0,0,0])
-        await vault.connect(user3).withdraw(await vault.balanceOf(user3.address), USDC.address, [0,0,0,0])
+        await vault.connect(user1).withdraw(await vault.balanceOf(user1.address), USDC.address, [0,0,0,0,0])
+        await vault.connect(user2).withdraw(await vault.balanceOf(user2.address), USDC.address, [0,0,0,0,0])
+        await vault.connect(user3).withdraw(await vault.balanceOf(user3.address), USDC.address, [0,0,0,0,0])
 
         console.log("User1 Withdrawn: ", ethers.utils.formatEther(await USDC.balanceOf(user1.address)))
         console.log("User2 Withdrawn: ", ethers.utils.formatEther(await USDC.balanceOf(user2.address)))
@@ -219,7 +219,7 @@ describe("Citadel - USDC", async () => {
         expect(vault.connect(user3).deposit(user3Balance, USDC.address)).to.be.revertedWith('Pausable: paused')
         expect(vault.connect(adminSigner).invest()).to.be.revertedWith('Pausable: paused')
 
-        await vault.connect(user3).withdraw(await vault.balanceOf(user3.address), USDC.address, [0,0,0,0])
+        await vault.connect(user3).withdraw(await vault.balanceOf(user3.address), USDC.address, [0,0,0,0,0])
         console.log("User3 Withdrawn: ", ethers.utils.formatEther(await USDC.balanceOf(user3.address)))
         
         console.log("value in pool -reinvest ", (await vault.getAllPoolInUSD()).toString())
