eb237a9474d6476ebc6fb3d44c290bd383897e0a
Soon Lai <soonlai814@gmail.com>
2021-10-15 23:01:41 +0800

    Update address

--- deploy/daoDegen/mainnet/verify.js
+++ deploy/daoDegen/mainnet/verify.js
@@ -1,18 +1,18 @@
 const { run } = require("hardhat")
 
-const vaultImpl = ""
-const strategyImpl = ""
+const vaultImpl = "0x84EEc4e9BEC17f2b46981665EE3A583C41d62E6f"
+const strategyImpl = "0xC4f5828211e776de812F456b326bD66A792C5534"
 
 module.exports = async () => {
 
     await run("verify:verify", {
         address: strategyImpl,
-        contract: "contracts/safu/DaoDegenStrategy.sol:DaoDegenStrategy"
+        contract: "contracts/daoDegen/DaoDegenStrategy.sol:DaoDegenStrategy"
     })
 
     await run("verify:verify", {
         address: vaultImpl,
-        contract: "contracts/safu/DaoDegenVault.sol:DaoDegenVault"
+        contract: "contracts/daoDegen/DaoDegenVault.sol:DaoDegenVault"
     })
 
 }
