4dabb8bc7daf2df06d6948d59c06d91c3834f949
seshanthS <>
2021-11-12 13:46:15 +0530

    update strategyUpgrade script

--- deploy/mainnet/upgrade.js
+++ deploy/mainnet/upgrade.js
@@ -1,9 +1,9 @@
 const { ethers, network, artifacts } = require("hardhat")
 const { mainnet: addresses } = require("../../addresses/citadelBSC")
 
-const proxyAdminAddr = "0x72eB6E3f163E8CFD1Ebdd7B2f4ffB60b6e420448" //"0x87C079A8Eb5459aaCe1f31E125C8992Df6Ff2168"
-const safuStrategyProxy = "0xdac6E0CD7A535038f5d836155784603FaC1ba23d" //"0x81390703430015A751F967694d5CCb8745FdA254"
-
+const proxyAdminAddr = "0x87C079A8Eb5459aaCe1f31E125C8992Df6Ff2168" // mainnet - "0x72eB6E3f163E8CFD1Ebdd7B2f4ffB60b6e420448"
+const safuStrategyProxy = "0x7436297148faCE594f1b2f04a2901c3bB65Eb771" // mainnet - "0xdac6E0CD7A535038f5d836155784603FaC1ba23d" 
+// const deployerAddress = "0x3f68a3c1023d736d8be867ca49cb18c543373b99"
 module.exports = async () => {
     const [deployer] = await ethers.getSigners() 
 
@@ -18,22 +18,30 @@ module.exports = async () => {
     const DaoSafuStrategy = await ethers.getContractFactory("DaoSafuStrategy")
     const daoSafuStrategy = await DaoSafuStrategy.deploy()
 
-    console.log("New implementation contract for DaoSafuVault:", daoSafuStrategy.address)
+    await daoSafuStrategy.wait()
+    console.log("New implementation contract for DaoSafuStrategy:", daoSafuStrategy.address)
 
-    const proxyAdmin = new ethers.Contract(proxyAdminAddr, [
-        "function upgradeAndCall(address proxy, address implementation, bytes memory data) public payable"
-    ], deployer)
+    // const proxyAdmin = new ethers.Contract(proxyAdminAddr, [
+    //     "function upgradeAndCall(address proxy, address implementation, bytes memory data) public payable"
+    // ], deployer)
 
+    // let artifacts_strategy = await artifacts.readArtifact("DaoSafuStrategy")
+    // let abi = artifacts_strategy.abi
+    // let interface = new ethers.utils.Interface(abi)
+    // let data = interface.encodeFunctionData("postUpgrade", [])
 
-    let artifacts_strategy = await artifacts.readArtifact("DaoSafuStrategy")
-    let abi = artifacts_strategy.abi
-    let interface = new ethers.utils.Interface(abi)
-    let data = interface.encodeFunctionData("postUpgrade", [])
+    // await proxyAdmin.upgradeAndCall(safuStrategyProxy, daoSafuStrategy.address, data)
 
     // const str = await ethers.getContractAt("DaoSafuStrategy", safuStrategyProxy) //todo remove
     // let ownerBeforeUpgrade = await str.owner()
 
-    await proxyAdmin.upgradeAndCall(safuStrategyProxy, daoSafuStrategy.address, data)
+    const proxyAdmin = new ethers.Contract(proxyAdminAddr, [
+        "function upgrade(address, address) external"
+    ], deployer)
+
+    await proxyAdmin.upgrade(safuStrategyProxy, daoSafuStrategy.address)
+
+
     
     // console.log("owner", ownerBeforeUpgrade, await str.owner())
     
